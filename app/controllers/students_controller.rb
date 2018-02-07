67e4a3281b3cd6c4a41da7483e1cec47dd94f0a2
Brittany Morris <brittmmendez@gmail.com>
2018-02-07 01:48:50 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
 
   def activate
    @student = set_student
-   @student.update(active: !@student.activ)
+   @student.update(active: !@student.active)
    redirect_to student_path(@student)
  end
 
